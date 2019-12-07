3dae95c8e3f9b8a5e96bf5915735df77c8f25669
Maurice Perry <maurice@perry.productions>
2019-12-07 20:56:24 +0100

    Use characteristic notification instead of polling

--- app/src/main/java/org/tastefuljava/flipit/MainActivity.java
+++ app/src/main/java/org/tastefuljava/flipit/MainActivity.java
@@ -1,37 +1,25 @@
 package org.tastefuljava.flipit;
 
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattService;
-import android.bluetooth.BluetoothManager;
-import android.bluetooth.BluetoothProfile;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import org.tastefuljava.flipit.device.DeviceConnection;
 import org.tastefuljava.flipit.server.Facet;
 import org.tastefuljava.flipit.server.ServerConnection;
 import org.tastefuljava.flipit.server.User;
 
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
@@ -48,9 +36,9 @@ public class MainActivity extends AppCompatActivity {
 
     private BluetoothAdapter bluetoothAdapter;
     private PentaView pentaView;
-    private Handler handler = new Handler();
     private ServerConnection cnt;
     private User user;
+    private DeviceConnection device;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,12 +48,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                connect(view);
-            }
-        });
+        fab.setOnClickListener(this::connect);
         pentaView = findViewById(R.id.pentaView);
         cnt = ServerConnection.open("maurice@perry.ch", "test1234");
         user = cnt.currentUser();
@@ -75,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        pentaView.setText("\uf45d");
+        pentaView.setText("");
     }
 
     @Override
@@ -109,141 +92,36 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             String address = intent.getStringExtra("deviceAddress");
-            connect(address);
+            try {
+                connect(address);
+            } catch (IOException e) {
+                showError("Error", e.getMessage());
+            }
         }
     };
 
-    private BluetoothGattCallback gattCallback =
-            new BluetoothGattCallback() {
-                int faceNumber = -1;
+    private void showError(String title, String message) {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                (dialog, which) -> dialog.dismiss());
+        alertDialog.show();
+    }
 
-                @Override
-                public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-                    if (status == BluetoothGatt.GATT_SUCCESS
-                            && newState == BluetoothProfile.STATE_CONNECTED) {
-                        Log.i(TAG, "Connected to gatt " + gatt.getDevice());
-                        Log.i(TAG, "Attempting to start service discovery:" +
-                                gatt.discoverServices());
-                    }
-                }
-
-                @Override
-                public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-                    if (status == BluetoothGatt.GATT_SUCCESS) {
-                        BluetoothGattService serv = gatt.getService(TIMEFLIP_ID);
-                        if (serv == null) {
-                            Log.i(TAG, "Not TimeFlip");
-                        } else {
-                            Log.i(TAG, "TimeFlip found");
-                            BluetoothGattCharacteristic characteristic
-                                    = serv.getCharacteristic(PASSWORD_CHARACTERISTIC);
-                            characteristic.setValue("000000".getBytes(StandardCharsets.US_ASCII));
-                            gatt.writeCharacteristic(characteristic);
-                            Log.i(TAG, "Password write requested");
-                        }
-                    }
-                }
-
-                @Override
-                public void onCharacteristicWrite(BluetoothGatt gatt,
-                                                  BluetoothGattCharacteristic characteristic,
-                                                  int status) {
-                    if (status == BluetoothGatt.GATT_SUCCESS
-                            && characteristic.getUuid().equals(PASSWORD_CHARACTERISTIC)) {
-                        Log.i(TAG, "Password written");
-                        BluetoothGattService serv = gatt.getService(TIMEFLIP_ID);
-                        BluetoothGattCharacteristic charact
-                                = serv.getCharacteristic(FACET_CHARACTERISTIC);
-                        gatt.readCharacteristic(charact);
-//                        gatt.setCharacteristicNotification(charact, true);
-                    }
-                }
-
-                @Override
-                public void onCharacteristicRead(final BluetoothGatt gatt,
-                                                 final BluetoothGattCharacteristic characteristic,
-                                                 int status) {
-                    Log.i(TAG, "onCharacteristicRead");
-                    if (status == BluetoothGatt.GATT_SUCCESS
-                            && characteristic.getUuid().equals(FACET_CHARACTERISTIC)) {
-                        Log.i(TAG, "Facet props: " + String.format("%02X", characteristic.getProperties()));
-                        byte[] data = characteristic.getValue();
-                        Log.i(TAG, "Facet length: " + data.length);
-                        if (data.length == 1) {
-                            Log.i(TAG, "Facet value: " + data[0]);
-                            facetChanged(data[0]);
-                        }
-                        handler.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                gatt.readCharacteristic(characteristic);
-                            }
-                        }, 2000);
-                    }
-                }
-
-                private void facetChanged(int face) {
-                    face %= 12;
-                    if (faceNumber != face) {
-                        faceNumber = face;
-                        Facet facet = null;
-                        if (user != null) {
-                            facet = user.getFacet(face);
-                        }
-                        if (facet == null || facet.getSymbol() == null) {
-                            pentaView.setText("");
-                        } else {
-                            pentaView.setText(facet.getSymbol());
-                        }
-                        cnt.sendFacet(face);
-                    }
-                }
-
-                private void sendFacet(String face) {
-                    try {
-                        Log.i(TAG, "Sending request");
-                        URL url = new URL("http://perry.ch/flipit-server/api/activity/log");
-                        HttpURLConnection cnt = (HttpURLConnection) url.openConnection();
-                        cnt.setDoOutput(true);
-                        cnt.setRequestProperty("Content-Type", "application/json");
-                        cnt.setRequestMethod("POST");
-                        try (Writer writer = new OutputStreamWriter(cnt.getOutputStream(),
-                                StandardCharsets.UTF_8)) {
-                            String json = "{\"username\":\"briner\",\"facename\":\"" + face + "\"}";
-                            Log.i(TAG, json);
-                            writer.write(json);
-                        }
-                        int st = cnt.getResponseCode();
-                        if (st >= 200 && st <= 299) {
-                            Log.i(TAG, "Request sent");
-                        } else {
-                            Log.e(TAG, "Request error: " + st);
-                        }
-                    } catch (IOException e) {
-                        Log.e(TAG, "sendFace", e);
-                    }
-                }
-
-                @Override
-                public synchronized void onCharacteristicChanged(BluetoothGatt gatt,
-                                                                 BluetoothGattCharacteristic characteristic) {
-                    Log.i(TAG, "onCharacteristicChanged");
-                    Log.i(TAG, "New value: " + characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT8, 1));
-                }
-            };
-
-    private void connect(String address) {
-        final BluetoothManager bluetoothManager =
-                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
-        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
-        if (device == null) {
-            Log.e(TAG, "Could not get remote device");
-        } else {
-            Log.i(TAG, "got remote device");
-            BluetoothGatt gatt = device.connectGatt(this, true, gattCallback);
-            Log.i(TAG, "connection requested: " + gatt);
-        }
+    private void connect(String address) throws IOException {
+        device = DeviceConnection.open(this, address, (face) -> {
+            Facet facet = null;
+            if (user != null) {
+                facet = user.getFacet(face);
+            }
+            if (facet == null || facet.getSymbol() == null) {
+                pentaView.setText("");
+            } else {
+                pentaView.setText(facet.getSymbol());
+            }
+            cnt.sendFacet(face);
+        });
     }
 
     public void connect(View view) {
---
+++ app/src/main/java/org/tastefuljava/flipit/device/DeviceConnection.java
@@ -0,0 +1,150 @@
+package org.tastefuljava.flipit.device;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+import static android.bluetooth.BluetoothGattCharacteristic.FORMAT_UINT8;
+
+public class DeviceConnection {
+    private static final String TAG = DeviceConnection.class.getSimpleName();
+
+    private static final UUID TIMEFLIP_ID
+            = UUID.fromString("F1196F50-71A4-11E6-BDF4-0800200C9A66");
+    private static final UUID ACCEL_CHARACTERISTIC
+            = UUID.fromString("F1196F51-71A4-11E6-BDF4-0800200C9A66");
+    private static final UUID FACET_CHARACTERISTIC
+            = UUID.fromString("F1196F52-71A4-11E6-BDF4-0800200C9A66");
+    private static final UUID FACET_DESCRIPTOR
+            = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
+    private static final UUID PASSWORD_CHARACTERISTIC
+            = UUID.fromString("F1196F57-71A4-11E6-BDF4-0800200C9A66");
+
+    private final Context context;
+    private final Callback callback;
+
+    public interface Callback {
+        void facedChanged(int newFacet);
+    }
+
+    public static DeviceConnection open(Context context, String address, Callback callback)
+            throws IOException {
+        DeviceConnection cnt = new DeviceConnection(context, callback);
+        cnt.open(address);
+        return cnt;
+    }
+
+    private DeviceConnection(Context context, Callback callback) {
+        this.context = context;
+        this.callback = callback;
+    }
+
+    private void open(String address) throws IOException {
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
+        if (device == null) {
+            String msg = "Could not get remote device [" + address + "]";
+            Log.e(TAG, msg);
+            throw new IOException(msg);
+        } else {
+            Log.i(TAG, "got remote device");
+            BluetoothGatt gatt = device.connectGatt(context, true, gattCallback);
+            Log.i(TAG, "connection requested: " + gatt);
+        }
+    }
+
+    private BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        int faceNumber = -1;
+
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            if (status == BluetoothGatt.GATT_SUCCESS
+                    && newState == BluetoothProfile.STATE_CONNECTED) {
+                Log.i(TAG, "Connected to gatt " + gatt.getDevice());
+                Log.i(TAG, "Attempting to start service discovery:" +
+                        gatt.discoverServices());
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                BluetoothGattService serv = gatt.getService(TIMEFLIP_ID);
+                if (serv == null) {
+                    Log.i(TAG, "Not TimeFlip");
+                } else {
+                    Log.i(TAG, "TimeFlip found");
+                    BluetoothGattCharacteristic characteristic
+                            = serv.getCharacteristic(PASSWORD_CHARACTERISTIC);
+                    characteristic.setValue("000000".getBytes(StandardCharsets.US_ASCII));
+                    gatt.writeCharacteristic(characteristic);
+                    Log.i(TAG, "Password write requested");
+                }
+            }
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt,
+                                          BluetoothGattCharacteristic characteristic,
+                                          int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS
+                    && characteristic.getUuid().equals(PASSWORD_CHARACTERISTIC)) {
+                Log.i(TAG, "Password written");
+                BluetoothGattService serv = gatt.getService(TIMEFLIP_ID);
+                BluetoothGattCharacteristic charact
+                        = serv.getCharacteristic(FACET_CHARACTERISTIC);
+                for (BluetoothGattDescriptor desc: charact.getDescriptors()) {
+                    System.out.println(desc.getUuid());
+                }
+                setCharacteristicNotification(gatt, charact, true);
+//                gatt.setCharacteristicNotification(charact, true);
+                gatt.readCharacteristic(charact);
+//                        gatt.setCharacteristicNotification(charact, true);
+            }
+        }
+
+        private boolean setCharacteristicNotification(BluetoothGatt bluetoothGatt, BluetoothGattCharacteristic characteristic,boolean enable) {
+            Log.d(TAG, "setCharacteristicNotification");
+            bluetoothGatt.setCharacteristicNotification(characteristic, enable);
+            BluetoothGattDescriptor descriptor = characteristic.getDescriptor(FACET_DESCRIPTOR);
+            descriptor.setValue(enable
+                    ? BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
+                    : BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE);
+            return bluetoothGatt.writeDescriptor(descriptor); //descriptor write operation successfully started?
+
+        }
+
+        private void facetChanged(int face) {
+            face %= 12;
+            if (faceNumber != face) {
+                faceNumber = face;
+                callback.facedChanged(face);
+            }
+        }
+
+        @Override
+        public synchronized void onCharacteristicChanged(BluetoothGatt gatt,
+                                                         BluetoothGattCharacteristic characteristic) {
+            Log.i(TAG, "onCharacteristicChanged");
+            Integer facet = characteristic.getIntValue(FORMAT_UINT8, 0);
+            Log.i(TAG, "New value: " + facet);
+            if (facet != null) {
+                facetChanged(facet);
+            }
+        }
+    };
+}
